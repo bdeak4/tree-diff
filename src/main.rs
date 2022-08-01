fc7998a1489a0081a48fadf00573da91bb1fc3d8
Bartol Deak <b@bdeak.net>
2022-08-01 23:22:23 +0200

    recursive print file names

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+    for entry in get_files(".") {
+        println!("{}", entry);
+    }
+}
+
+fn get_files(root: &str) -> Vec<String> {
+    WalkDir::new(root)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file())
+        .map(|e| e.path().to_str().unwrap().to_string())
+        .collect()
 }
