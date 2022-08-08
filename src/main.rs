46904b25ef837a3b7abebe84aae53c7a2042f141
Bartol Deak <b@bdeak.net>
2022-08-08 23:17:14 +0200

    print file hashes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use sha2::{Digest, Sha256};
+use std::{fs::File, io};
 use walkdir::WalkDir;
 
 fn main() {
     for entry in get_files(".") {
         println!("{}", entry);
+        get_hash(entry)
     }
 }
 
@@ -13,4 +16,13 @@ fn get_files(root: &str) -> Vec<String> {
         .filter(|e| e.file_type().is_file())
         .map(|e| e.path().to_str().unwrap().to_string())
         .collect()
+}
+
+fn get_hash(filename: String) {
+    let mut hasher = Sha256::new();
+    let mut file = File::open(filename).unwrap();
+    let bytes_written = io::copy(&mut file, &mut hasher).unwrap();
+    let hash_bytes = hasher.finalize();
+
+    println!("hash is: {:x}", hash_bytes);
 }
