50e49c87bfe45ad67e04bdf9b0173ff2280c2d5c
Bartol Deak <b@bdeak.net>
2022-08-21 12:50:05 +0200

    use blake hashing algorithm for hashing

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,51 @@
-use sha2::{Digest, Sha256};
-use std::{fs::File, io};
-use walkdir::WalkDir;
+use blake2::{Blake2s256, Digest};
+use std::path::Path;
+use std::{fs, io};
 
 fn main() {
-    for entry in get_files(".") {
-        println!("{}", entry);
-        get_hash(entry)
-    }
+    println!("hi");
+    println!("{}", get_hash(Path::new("./src/main.rs")).unwrap());
 }
 
-fn get_files(root: &str) -> Vec<String> {
-    WalkDir::new(root)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| e.file_type().is_file())
-        .map(|e| e.path().to_str().unwrap().to_string())
-        .collect()
+fn get_files() {
+    println!("get files")
 }
 
-fn get_hash(filename: String) {
-    let mut hasher = Sha256::new();
-    let mut file = File::open(filename).unwrap();
-    let bytes_written = io::copy(&mut file, &mut hasher).unwrap();
-    let hash_bytes = hasher.finalize();
-
-    // show only first 7 chars of hash
-
-    println!("hash is: {:x}", hash_bytes);
+fn get_hash(path: &Path) -> Result<String, io::Error> {
+    let mut file = fs::File::open(&path)?;
+    let mut hasher = Blake2s256::new();
+    io::copy(&mut file, &mut hasher)?;
+    let hash = format!("{:x}", hasher.finalize());
+    let hash_short = hash[..7].to_string();
+    Ok(hash_short)
 }
+
+// TODO: walkdir
+//
+// fn main() -> io::Result<()> {
+//     visit_dirs(".", |entry: DirEntry| {
+//         let path = entry.path();
+//         if path.is_file() {
+//             println!("{}", path.display());
+//         }
+//     })?;
+
+//     // The entries have now been sorted by their path.
+
+//     Ok(())
+// }
+
+// fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
+//     if dir.is_dir() {
+//         for entry in fs::read_dir(dir)? {
+//             let entry = entry?;
+//             let path = entry.path();
+//             if path.is_dir() {
+//                 visit_dirs(&path, cb)?;
+//             } else {
+//                 cb(&entry);
+//             }
+//         }
+//     }
+//     Ok(())
+// }
